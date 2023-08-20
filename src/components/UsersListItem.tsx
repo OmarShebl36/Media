c73f9e1f8bcc1c9793900d6282f1cd56a7512eca
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 16:53:03 +0300

    feat: add trash icon and make use of deleteUser thunk

--- src/components/UsersListItem.tsx
+++ src/components/UsersListItem.tsx
@@ -1,13 +1,30 @@
-import { User } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+import { GoTrash } from 'react-icons/go';
+import { User, deleteUser } from '../store';
+import Button from './Button';
 
 interface Props {
   user: User;
 }
 
 function UsersListItem({ user }: Props) {
+  const [doDeleteUser, isLoading, error] = useThunk(deleteUser);
+
+  if (typeof doDeleteUser !== 'function') {
+    return null;
+  }
+
+  const handleClick = () => {
+    doDeleteUser(user);
+  };
+
   return (
     <div className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
+      <div className='flex p-2 justify-start items-center cursor-pointer'>
+        <Button loading={isLoading as boolean} onClick={() => handleClick()}>
+          <GoTrash />
+        </Button>
+        {error && <div>Error deleting user.</div>}
         {user.name}
       </div>
     </div>
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -10,3 +10,4 @@ export const store = configureStore({
 export type AppDispatch = typeof store.dispatch
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
+export * from './thunks/deleteUser';
