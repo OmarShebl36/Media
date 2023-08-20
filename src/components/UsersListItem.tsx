9cf6fbc16e7161432c5e2d5d54932c5f39014146
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 16:58:34 +0300

    feat: fix the style of the user item

--- src/components/UsersListItem.tsx
+++ src/components/UsersListItem.tsx
@@ -20,12 +20,14 @@ function UsersListItem({ user }: Props) {
 
   return (
     <div className='mb-2 border rounded'>
-      <div className='flex p-2 justify-start items-center cursor-pointer'>
-        <Button loading={isLoading as boolean} onClick={() => handleClick()}>
-          <GoTrash />
-        </Button>
-        {error && <div>Error deleting user.</div>}
-        {user.name}
+      <div className='flex p-2 justify-between items-center cursor-pointer'>
+        <div className='flex flex-row items-center justify-between'>
+          <Button className="mr-3" loading={isLoading as boolean} onClick={() => handleClick()}>
+            <GoTrash />
+          </Button>
+          {error && <div>Error deleting user.</div>}
+          {user.name}
+        </div>
       </div>
     </div>
   );
