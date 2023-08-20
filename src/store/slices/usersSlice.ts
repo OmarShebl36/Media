a88f37975737490f0c881466bbe50a93ee9bd6b6
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 10:45:46 +0300

    feat: create async thunk and make use of it

--- src/store/slices/usersSlice.ts
+++ src/store/slices/usersSlice.ts
@@ -1,4 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { SerializedError, createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
+
+interface InitialValues {
+  data: {id: number, name: string}[];
+  isLoading: boolean;
+  error: SerializedError | null;
+}
 
 const usersSlice = createSlice({
   name: 'users',
@@ -6,8 +13,21 @@ const usersSlice = createSlice({
     data: [],
     error: null,
     isLoading: false,
-  },
+  } as InitialValues,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
