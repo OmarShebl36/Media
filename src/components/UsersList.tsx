aed5a3a7a766e9cadd5528fca8ff9640f17e335b
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 13:00:55 +0300

    feat: create  userList ui

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, User, fetchUsers } from '../store';
+import { AppDispatch, User, addUser, fetchUsers } from '../store';
 import Skeleton from './Skeleton';
+import Button from './Button';
 
 function UsersList() {
   const dispatch = useDispatch<AppDispatch>();
@@ -13,8 +14,12 @@ function UsersList() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleUserAdded = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) {
-    return <Skeleton times={6} className='h-10 w-full'/>;
+    return <Skeleton times={6} className='h-10 w-full' />;
   }
 
   if (error) {
@@ -22,14 +27,24 @@ function UsersList() {
   }
 
   const renderedUsers = data.map((user: User) => {
-    return <div key={user.id} className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        {user.name}
+    return (
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
       </div>
-    </div>
-  })
+    );
+  });
 
-  return <div>{renderedUsers}</div>;
+  return (
+    <div>
+      <div className='flex flex-row justify-between m-3'>
+        <h1 className='m-2 text-xl'>Users</h1>
+        <Button onClick={handleUserAdded}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
