aff05d20b1fef546761b23ae2fad571f74c3f79f
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 10:14:49 +0300

    feat: prepare App to show UsersList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import UsersList from './components/UsersList';
 
 function App() {
   return (
-    <div>
-      hello!
+    <div className='container ms-auto'>
+      <UsersList />
     </div>
   );
 }
