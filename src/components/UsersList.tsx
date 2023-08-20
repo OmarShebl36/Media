68c1a3fda5589dd6cb0240a58e1b2a10afc6c9dd
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 16:29:55 +0300

    rfr: refactor usersList to show the header in every case

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -30,36 +30,38 @@ function UsersList() {
     doCreateUser();
   };
 
-  if (isLoadingUsers) {
-    return <Skeleton times={6} className='h-10 w-full' />;
-  }
-
   if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
-  const renderedUsers = data.map((user: User) => {
-    return (
-      <div key={user.id} className='mb-2 border rounded'>
-        <div className='flex p-2 justify-between items-center cursor-pointer'>
-          {user.name}
+  let content: JSX.Element;
+
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className='h-10 w-full' />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user: User) => {
+      return (
+        <div key={user.id} className='mb-2 border rounded'>
+          <div className='flex p-2 justify-between items-center cursor-pointer'>
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className='flex flex-row justify-between m-3'>
+      <div className='flex flex-row justify-between items-center m-3'>
         <h1 className='m-2 text-xl'>Users</h1>
-        {isCreatingUser ? (
-          'Creating User...'
-        ) : (
-          <Button onClick={handleUserAdded}>+ Add User</Button>
-        )}
+        <Button loading={isCreatingUser as boolean} onClick={handleUserAdded}>
+          + Add User
+        </Button>
         {creatingUserError && 'Error creating user...'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
