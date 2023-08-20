6ee03a1f2cce57ef7b8b6dd43deedb667c03bb5e
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 11:23:24 +0300

    feat: add logic to userslist component

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,14 +1,26 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, fetchUsers } from '../store';
 
 function UsersList() {
   const dispatch = useDispatch<AppDispatch>();
+  const { isLoading, data, error } = useSelector((state: any) => {
+    return state.users;
+  });
+
   useEffect(() => {
-   dispatch(fetchUsers()); 
+    dispatch(fetchUsers());
   }, [dispatch]);
 
-  return <div>UsersList</div>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
