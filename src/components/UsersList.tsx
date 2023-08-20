79f18211159fb2d7ed0a4ace0920dea72d40c7aa
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-20 12:28:20 +0300

    feat: create Skeleton animation and make use of it

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, fetchUsers } from '../store';
+import { AppDispatch, User, fetchUsers } from '../store';
+import Skeleton from './Skeleton';
 
 function UsersList() {
   const dispatch = useDispatch<AppDispatch>();
@@ -13,14 +14,22 @@ function UsersList() {
   }, [dispatch]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Skeleton times={6} className='h-10 w-full'/>;
   }
 
   if (error) {
     return <div>Error fetching data...</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user: User) => {
+    return <div key={user.id} className='mb-2 border rounded'>
+      <div className='flex p-2 justify-between items-center cursor-pointer'>
+        {user.name}
+      </div>
+    </div>
+  })
+
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
