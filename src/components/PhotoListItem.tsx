6564b51f829ee89391e00dc4ee7087b5d79ad268
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-22 19:49:34 +0300

    feat: show images and delete buttons

--- src/components/PhotoListItem.tsx
+++ src/components/PhotoListItem.tsx
@@ -1,11 +1,19 @@
 import { Photo } from '../types';
 
 interface Props {
-    photo: Photo;
+  photo: Photo;
 }
 
-function PhotoListItem({photo}: Props) {
-    return ( <>{photo.url}</> );
+function PhotoListItem({ photo }: Props) {
+  return (
+    <div className='relative m-2'>
+      <img
+        className='h-20 w-20'
+        src={photo.url}
+        alt={`Random faker ${photo.id}`}
+      />
+    </div>
+  );
 }
 
 export default PhotoListItem;
