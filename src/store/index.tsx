2be6afa2d2ca48e0a705302e8c8fa61671a0f299
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-22 19:30:05 +0300

    feat: add photosApi middlewate and reducer

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,14 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import { usersReducer } from './slices/usersSlice';
 import { albumsApi } from './apis/albumsApi';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { photosApi } from './apis/photosApi';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(albumsApi.middleware),
+    getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware),
 });
 
 setupListeners(store.dispatch);
